a09f3ac4ca4e5c17a713f163f65eb60494cf689c
choisuhyeon <csh@skypay.co.kr>
2026-01-07 12:21:00 +0900

    Velo Nuget Update

--- VeloUpdateSystem/App.xaml.cs
+++ VeloUpdateSystem/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Velopack;
 
 namespace VeloUpdateSystem
 {
@@ -9,6 +10,14 @@ namespace VeloUpdateSystem
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            VelopackApp.Build()
+                .SetArgs(e.Args)
+                .Run();
+
+            base.OnStartup(e);
+        }
     }
 
 }
